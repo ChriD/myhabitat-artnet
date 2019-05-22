23e8764a0b2548b2a8e74272114f3c0e02a29191
Christian Dürnberger <chris_d85@hotmail.com>
2019-05-22 20:47:59 +0200

    0.0.1

--- src/nodes/things/artnet/myhabitat.node.thing.artnet.light.js
+++ src/nodes/things/artnet/myhabitat.node.thing.artnet.light.js
@@ -1,6 +1,7 @@
 "use strict"
 
-const MyHabitatNode_Thing_ARTNET   = require('./myhabitat.node.thing.artnet.js')
+const MyHabitatNode_Thing_ARTNET  = require('./myhabitat.node.thing.artnet.js')
+const CloneDeep                   = require('lodash.clonedeep')
 
 
 module.exports = function(RED) {
@@ -54,6 +55,11 @@ module.exports = function(RED) {
           if(payload.toUpperCase() === "TOGGLE")
           _message.state.isOn = this.state().isOn ? false : true
           break
+        case "object":
+          // we may get a direct color object in the payload
+          if(payload.color)
+            _message.state.color = payload.color
+          break
       }
 
       // apply the state object which was given by the input or which was created from
@@ -64,8 +70,10 @@ module.exports = function(RED) {
           this.turnOn()
         if(!_message.state.isOn)
           this.turnOff()
-        if(_message.state.brightness && this.isDimmable())
-          this.setBrightness(_message.state.brightness / 100 * 255)
+        if(_message.state.brightness)
+          this.setBrightness(_message.state.brightness)
+        if(_message.state.color)
+          this.setColor(_message.state.color)
       }
 
       this.updateNodeInfoStatus()
@@ -73,24 +81,69 @@ module.exports = function(RED) {
     }
 
 
-    isDimmable()
+    hasRGB()
     {
-      return this.config.lightType === "SIMPLEDIM" ? true : false
+      return (this.config.lightType === "RGB" || this.config.lightType === "RGBW") ? true : false
+    }
+
+    hasWhite()
+    {
+      return (this.config.lightType === "SIMPLE" || this.config.lightType === "RGBW" || this.config.lightType === "TW") ? true : false
+    }
+
+    hasWarmWhite()
+    {
+      return (this.config.lightType === "TW") ? true : false
+    }
+
+    updateArtnetValues(_color, _brightness)
+    {
+      if(this.hasWhite())
+      {
+        this.artnetFade(this.config.channelWhite,     _color.white      * (_brightness / 100))
+      }
+      if(this.hasWarmWhite())
+      {
+        this.artnetFade(this.config.channelWarmWhite, _color.warmwhite  * (_brightness / 100))
+      }
+      if(this.hasRGB())
+      {
+        this.artnetFade(this.config.channelRed,       _color.red        * (_brightness / 100))
+        this.artnetFade(this.config.channelGreen,     _color.green      * (_brightness / 100))
+        this.artnetFade(this.config.channelBlue,      _color.blue       * (_brightness / 100))
+      }
     }
 
 
     turnOn()
     {
+      // if we are switching on, we have to set the artnet value to the one given in the current state
+      this.updateArtnetValues(this.state().color, this.state().brightness)
+      this.state().isOn = true
     }
 
 
     turnOff()
     {
+      this.updateArtnetValues({ white       : 0,
+                                warmwhite   : 0,
+                                red         : 0,
+                                green       : 0,
+                                blue        : 0}, 0)
+      this.state().isOn = false
     }
 
 
+    setColor(_color)
+    {
+      this.updateArtnetValues(_color, this.state().brightness)
+      this.state().color = CloneDeep(_color)
+    }
+
     setBrightness(_brightness)
     {
+      this.updateArtnetValues(this.state().color, _brightness)
+      this.state().brightness = _brightness
     }
 
 
